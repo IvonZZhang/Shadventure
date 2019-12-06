4aa919d7dc790c92b56e35979bb4fe00eea8f296
ZZZoe <37062794+ZijunYin@users.noreply.github.com>
2019-12-06 18:29:08 +0100

    guard level added

---
+++ Shadventure/Assets/Scripts/sun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour
+{
+    public Transform guard;
+    public Transform guard_sleep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // guard_sleep.SetActive(guard_sleep);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBecameInvisible()
+    {
+        // Destroy(guard);
+        guard.position = new Vector3(-10.0f, -30.0f, 0);
+        // Visibility.Visible(guard_sleep);
+        guard_sleep.position = new Vector3(-41.0f, -4.5f, 0);
+    }
+}
