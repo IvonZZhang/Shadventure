6270dd2f2c341c2990105e7944dcea7ff64f3811
ChenZhang0116 <58338513+ChenZhang0116@users.noreply.github.com>
2019-12-10 08:47:32 +0100

    cloudboat

---
+++ Shadventure/Assets/Scripts/boat.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boat : MonoBehaviour
+{
+    public GameObject myShadow;
+    
+    private float triggerY = 0.75f;
+    private float stopX = -3.9f;
+    private float thrust = 100.0f;
+    private float shipSpeed = 2.0f;
+    private bool floatingForceOn = false;
+
+    private Rigidbody2D ship01Rigidbody;
+    private Collider2D ship01Collider;
+    private Collider2D shadowCollider;
+    private Vector2 floatingForce;
+    private Vector2 floatingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ship01Rigidbody = GetComponent<Rigidbody2D>();
+        ship01Collider = GetComponent<PolygonCollider2D>();
+        shadowCollider = myShadow.GetComponent<PolygonCollider2D>();
+        if(shadowCollider == null)
+        {
+            Debug.Log("shadowCollider is null!!!! \n");
+        }
+        floatingForce = new Vector2(0, 3);
+        floatingSpeed = new Vector2(0, 2);
+        ship01Rigidbody.constraints = UnityEngine.RigidbodyConstraints2D.FreezeRotation;
+        ship01Rigidbody.constraints = UnityEngine.RigidbodyConstraints2D.FreezePositionX;
+
+        Physics2D.IgnoreCollision(ship01Collider, shadowCollider);
+
+        Vector3 startPoint = new Vector3(-8.0f, -3.5f, 0);
+        transform.position = startPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y >= triggerY)
+        {
+            ship01Rigidbody.velocity = shipSpeed * transform.right;
+            if(transform.position.x >= stopX)
+            {
+                ship01Rigidbody.constraints = UnityEngine.RigidbodyConstraints2D.FreezeAll;
+            }
+        }
+    }   
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {   
+        ship01Rigidbody.constraints = UnityEngine.RigidbodyConstraints2D.FreezeRotation;
+        if(Physics2D.GetIgnoreCollision(ship01Collider, shadowCollider))
+        {
+            Debug.Log("--- collision between shadow & boat is ignored \n");
+        }
+
+        Collider2D collidee = collision.collider; 
+        if(collision.gameObject.tag == "Metaball_liquid")
+        {
+            //Debug.Log("contacting with water ---- \n");
+            //Physics2D.IgnoreCollision(ship01Collider, collidee, true);
+            ship01Rigidbody.velocity = floatingSpeed*transform.up;
+            //collision.enabled = false;
+            //Vector3 impulse = collision.impulse;
+            //ship01Rigidbody.AddForce(-impulse, ForceMode.Impulse);//refrain the boat from colliding impulses
+        }  
+        // if(collision.gameObject.tag == "Player")
+        // {
+        //     Debug.Log("ship01 colliding with player ---- \n");
+        // }      
+    }
+
+
+
+
+}
---
+++ Shadventure/Assets/Scripts/cloudRaining.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Water2D;
+
+public class cloudRaining : MonoBehaviour
+{
+    public float cloudSpeed = 10.0f;
+    public GameObject myPipe;
+    public Water2D_Spawner myPipeScript;
+
+    private float boundary = 10.0f;
+    private float height = 3.5f;
+    private Rigidbody2D cloudRigidbody2d;
+    private Vector3 Point1;
+    private Vector3 Point2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloudRigidbody2d = GetComponent<Rigidbody2D>();
+        Point1 = new Vector3(-boundary, height, 0);
+        Point2 = new Vector3(boundary, height, 0);
+        Vector3 startPoint = new Vector3(-boundary-20, height, 0);
+
+        transform.position = startPoint;
+        myPipe.transform.position = startPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        myPipe.transform.position = transform.position + new Vector3(0.5f, -0.5f, 0);
+        
+        if(transform.position.x < Point1.x)
+        {
+            cloudRigidbody2d.velocity = cloudSpeed * transform.right;
+            myPipeScript.Restore();
+        }
+        else if(transform.position.x > Point2.x)
+        {
+            cloudRigidbody2d.velocity = -cloudSpeed * transform.right;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "tilemapTag")
+        {
+            Debug.Log("cloud02 colliding with tilemap ---- \n");
+
+        }
+        else if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("cloud02 colliding with player ---- \n");
+            myPipeScript.Spawn();
+            cloudRigidbody2d.constraints = UnityEngine.RigidbodyConstraints2D.FreezeAll;
+        }
+        
+    }
+
+
+
+}
